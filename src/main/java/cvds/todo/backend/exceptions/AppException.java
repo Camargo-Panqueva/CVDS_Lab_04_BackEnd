8369e8df9b342acc398c5cae44f4824ad8cad394
KenjiroYamato <juanpablocamargo91@gmail.com>
2024-09-28 07:55:28 -0500

    feat: Add custom application exceptions and documentation.

---
+++ src/main/java/cvds/todo/backend/exceptions/AppException.java
@@ -0,0 +1,27 @@
+package cvds.todo.backend.exceptions;
+
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Abstract class representing a custom application exception.
+ * This class extends the standard Java Exception class and provides
+ * additional functionality for handling HTTP response status codes.
+ */
+public abstract class AppException extends Exception {
+
+    private final Integer statusCode;
+
+    public AppException(String message, Integer statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    /**
+     * Generates a ResponseEntity object with the HTTP status code and message.
+     *
+     * @return ResponseEntity containing the status code and exception message.
+     */
+    public ResponseEntity<Object> getResponse() {
+        return ResponseEntity.status(statusCode).body(this.getMessage());
+    }
+}
